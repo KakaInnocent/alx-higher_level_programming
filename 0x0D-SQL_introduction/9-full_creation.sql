ae916afd71c904b7f9fca6a109856de8b33fa51d
Mambo Innocent <saneugene847@gmail.com>
2022-09-06 04:47:08 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,8 +1,8 @@
---Query to add table second_table in database hbtn_0c_0
-CREATE TABLE IF NOT EXISTS second_table (
-id INT,
-name VARCHAR(256),
-score INT);
+--Query to create table 'second_table' in database 'hbtn_0c_0'
+-- (id INT), (name VARCHAR(256)), (score INT)
+CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+
+--Query to inster 4 records
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
