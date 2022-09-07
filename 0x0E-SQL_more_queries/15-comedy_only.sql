7831912ab98139bcc83eb22eb02da5475c4c4342
Mambo Innocent <saneugene847@gmail.com>
2022-09-07 07:28:29 -0700

    joins?

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,8 +1,9 @@
--- Query to list all comdy shows in hbtn_0d_tvshows database
-SELECT titke
+-- Query to list Dexter tv show genres
+SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_shows.id
-WHEER tv_genres.name = 'Comedy'
-GROUP BY title
-ORDER BY title ASC;
+JOIN tv_show_genres
+ON tv_show_genres.show_id = tv_shows.id
+JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
+WHERE tv_genres.name = 'Comedy'
+ORDER BY tv_shows.title;
