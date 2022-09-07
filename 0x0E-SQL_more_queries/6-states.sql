2dafbfdbba5789becaa320c917a002ca0aff3ae3
Mambo Innocent <saneugene847@gmail.com>
2022-09-07 05:19:54 -0700

    trial 1

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,7 @@
 --Query to create database hbtn_0d_usa if it doesn't exist
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 --Query to create states table with 2 constraints
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(
+CREATE TABLE IF NOT EXISTS states(
 		id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
 		name VARCHAR(256) NOT NULL);
