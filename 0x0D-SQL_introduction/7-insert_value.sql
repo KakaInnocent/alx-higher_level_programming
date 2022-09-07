b2132c843f34f9311738ffe42660bf65a806d6c2
Mambo Innocent <saneugene847@gmail.com>
2022-09-07 08:07:13 -0700

    task 8

--- 0x0D-SQL_introduction/7-insert_value.sql
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -1,2 +1,5 @@
---Query to insert a new row into first_table in hbtn_0c_0 database
-INSERT INTO first_table (id, name) VALUES (89, "Best School");
+-- Insert a new row in 'first_table' from db 'hbtn_0c_0'
+-- id == 89
+-- name == Holberton School
+-- db name will be passed as arg to mysql cmd
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
