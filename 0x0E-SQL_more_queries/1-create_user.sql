cfecc1e320476f67c050d6fabe8ec8d2010b7a21
Mambo Innocent <saneugene847@gmail.com>
2022-09-07 04:40:41 -0700

    create user privileges

---
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -0,0 +1,3 @@
+-- Query to create user 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+GRANT ALL PRIVILEGES ON * . * TO 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
