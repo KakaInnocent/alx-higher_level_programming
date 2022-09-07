96a0ec4a1ae388e0305b93931afb2a52fe369040
Mambo Innocent <saneugene847@gmail.com>
2022-09-07 08:06:03 -0700

    task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,9 +1,11 @@
---Query to create table 'second_table' in database 'hbtn_0c_0'
--- (id INT), (name VARCHAR(256)), (score INT)
-CREATE TABLE IF NOT EXISTS second_table (id INT,name VARCHAR(256),score INT);
+-- Create table 'second_table' in db 'hbtn_0c_0'
+-- Add descriptions (id INT), (name VARCHAR(256)), (score INT)
+-- db name will be passed as arg to mysql cmd
+-- Not allowed to use SELECT or SHOW
+CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 
---Query to inster 4 records
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, 'John', 10);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, 'Alex', 3);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, 'Bob', 14);
-INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, 'George', 8);
+-- Script should create 4 records
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (2, "Alex", 3);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (3, "Bob", 14);
+INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (4, "George", 8);
