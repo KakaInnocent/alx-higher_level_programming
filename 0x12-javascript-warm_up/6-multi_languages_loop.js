2b27b5d761db829761ea1538f8e0c3a1765eac97
Mambo Innocent <saneugene847@gmail.com>
2022-10-04 05:49:31 -0700

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for (let i = 0; i < strings.length; i++) {
-  console.log(strings[i]);
+const msg = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(msg[i]);
 }
